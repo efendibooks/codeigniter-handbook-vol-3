98aa2cb34e910fa0834d476f491e3370aac9acb4
Jamie Rumbelow <jamie@jamierumbelow.net>
2012-11-25 18:22:41 +0000

    Adding comments to the hook

--- application/hooks/phpunit.php
+++ application/hooks/phpunit.php
@@ -1,4 +1,12 @@
 <?php
+/**
+ * The CodeIgniter Handbook - Volume Three - Unit Testing
+ * https://efendibooks.com/books/codeigniter-handbook/vol-3
+ *
+ * hooks/phpunit.php - A silly CodeIgniter fix
+ *
+ * Copyright (c) 2012 Jamie Rumbelow <http://jamierumbelow.net>
+ */
 
 class PHPUnit_CodeIgniter_Hook
 {
